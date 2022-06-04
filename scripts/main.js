7361f9d8755eb8408e04b0ea1632fdcb91ea9da0
yanda <i.am.kitiket@gmail.com>
2022-06-04 22:39:12 +0200

    create main page

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+/* document.querySelector('html').onclick = function() {
+    alert('Ouch! Stop poking me!');
+}
+ */
+/* var myHTML = document.querySelector('html');
+myHTML.onclick = function() {}; */
+//it's the same
+
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute ('src','images/firefox2.png');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+    var myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    var storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+
+  myButton.onclick = function() {
+    setUserName();
+  }
+  
